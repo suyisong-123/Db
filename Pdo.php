b2e0b11b3e496c021104f4b5a474c560800eeecc
suyisong-123 <61648435+suyisong-123@users.noreply.github.com>
2021-03-01 19:24:14 +0800

    修改Pdo.php文件, 完善连接数据库

--- Pdo.php
+++ Pdo.php
@@ -6,17 +6,27 @@ use Db\DbInterface;
 class Pdo implements DbInterface{
 
     private $pdo;
+
     /**
-     * PDO constructor.
-     * @param $config 配置参数
+     * Pdo constructor.
+     * @param $config
+     * @throws \Exception 抛出连接数据库移除
      */
     public function __construct($config)
     {
-        $dsn = "";
-        $username = "";
-        $passwd = "";
-        $option = "";
-        $pdo = new \PDO($dsn, $username, $passwd, $option);
+        $dbname = $config['dbname']??'test';
+        $host = $config['host']??'127.0.0.1';
+        $charset = $config['charset']??'utf8';
+        $dsn = "mysql:dbname=".$dbname.";host=".$host.";charset=".$charset;
+        $username = $config['user']??'root';
+        $passwd = $config['password']??'forlove123';
+        $option = [];
+        try{
+            $pdo = new \PDO($dsn, $username, $passwd, $option);
+        }catch (\Exception $e){
+            throw $e;
+        }
+
     }
 
     public function addRecord($data)
